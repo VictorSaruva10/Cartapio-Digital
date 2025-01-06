8800b9cdb10b88c591db31b082c66777ebce3b72
Victor Hugo <saruvavictor@gmail.com>
2025-01-05 23:26:51 -0300

    teste whats

--- script.js
+++ script.js
@@ -157,22 +157,7 @@ addressInput.addEventListener("input", function(event){
 })
 
 checkoutBtn.addEventListener("click", function(){
-    const isOpen = checkOpen()
-    if(!isOpen){
-        
-        Toastify({
-            text: "O restaurante esta fechado",
-            duration: 3000,
-            close: true,
-            gravity: "top", // `top` or `bottom`
-            position: "right", // `left`, `center` or `right`
-            stopOnFocus: true, // Prevents dismissing of toast on hover
-            style: {
-              background: "#ef4444",
-            },
-        }).showToast()
-        return
-    }
+ 
     if(cart.length === 0)return
 
     if(addressInput.value ===""){
